fb331aa567c40b148fcf0f9ac2a550946425f673
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2012-10-16 01:31:10 -0300

    IMplementation of getters and settes of mundo class
    Signed-off-by: Marcos Paulo de Souza  <marcos.souza.org@gmail.com>

--- n3/mundo.h
+++ n3/mundo.h
@@ -29,9 +29,22 @@ class mundo {
 			this->y2 = y2;
 		}
 
+		void getOrtho(int *x1, int *y1, int *x2, int *y2) {
+			*x1 = this->x1;
+			*y1 = this->y1;
+			*x2 = this->x2;
+			*y2 = this->y2;
+		}
+
 		void setRGB(int r, int g, int b) {
 			this->r = r;
 			this->g = g;
 			this->b = b;
-		};
-}
+		}
+
+		void getRGB(int *r, int *g, int *b) {
+			*r = this->r;
+			*g = this->g;
+			*b = this->b;
+		}
+};
