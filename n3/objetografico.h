e0c1b5b6eb84cfd63b4358b78032aa972d2c654d
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2012-10-18 20:07:23 -0300

    Colocado atributo rgb denovo como protected
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- n3/objetografico.h
+++ n3/objetografico.h
@@ -9,12 +9,13 @@ class ObjetoGrafico
 	private:
 		int id;
 		vector<ObjetoGrafico> filhos;
-		/* cores do objeto */
-		int r, g, b;
 	public:
 		ObjetoGrafico(int);
 		virtual void desenha(void)=0;
 		int getId(void);
 		int addObjGrafFilho(ObjetoGrafico&);
 		int removeObjGrafFilho(int);
+	protected:
+		/* cores do objeto */
+		int r, g, b;
 };
